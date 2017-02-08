0e5e694d336e6d215223694e5c87ebf946229f76
micooo25 <jmsanluis25@gmail.com>
2017-02-08 12:59:45 +0800

    Add files via upload

---
+++ anyco.php
@@ -0,0 +1,33 @@
+<?php
+ 	require('anyco_ui.inc');
+
+
+	$conn = oci_connect('hr','hr','//localhost/XE');
+ 	ui_print_header('Department');
+	do_query($conn,'SELECT * FROM DEPARTMENTS');
+	ui_print_footer(date('Y-m-d H: i: s '));
+
+	function do_query($conn,$query){
+		$stid=oci_parse($conn,$query);
+		$r=oci_execute($stid,OCI_DEFAULT);
+		
+	print '<table border="1">';
+		while($row=oci_fetch_array($stid, OCI_RETURN_NULLS)){
+			print '<tr>';
+			foreach($row a $item){
+				print '<td>'.
+				($item ? htmlentities($item): '&nbsp,'). '</td>';
+			}
+			
+	print '</tr>';
+	
+		}
+		
+		print '</table>';
+		
+	}
+			
+
+	
+	
+?>
